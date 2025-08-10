5631c61e90f07002bb0ca48afb835e48e2672aab
Samuel Obillo <obillo.samuel@ncst.edu.ph>
2025-08-10 17:23:35 +0800

    Delete Enrollment System directory

--- Enrollment System/style/login.js
+++
@@ -1,8 +0,0 @@
- function message_status() {
-    Swal.fire({
-        title: "Login Successful",
-        text: "Welcome to the Enrollment System!",
-        icon: "success",
-        confirmButtonText: "OK"
-    });
-}
